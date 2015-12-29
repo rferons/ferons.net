ec21444e774599efc1bb9263beaf51fd2bdf2664
Ryan Ferons <rferons@gmail.com>
2015-12-29 15:46:54 -0500

    changed home page title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'home')
 
 @section('content')
-    <h1 class="center-align">Ferons Family Photos</h1>
+    <h1 class="center-align">Ferons Family</h1>
     <div class="row">
         <div class="col s6 m4 l2">
             <img height="400" class="materialboxed" data-caption="A picture of some deer and tons of trees" width="250" src="http://th01.deviantart.net/fs70/PRE/i/2013/126/1/e/nature_portrait_by_pw_fotografie-d63tx0n.jpg">                                    
